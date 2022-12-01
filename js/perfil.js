c20af90febb3f9dbd604381e7aaeffe16ae7b151
Cadu <99202990+Cadub@users.noreply.github.com>
2022-12-01 16:05:52 -0300

    Update perfil.js

--- js/perfil.js
+++ js/perfil.js
@@ -97,3 +97,18 @@ async function mostrarDados(){
     nome_usuario.textContent = "Nome Usuário: " + nome
     email_usuario.textContent = "E-mail: " + email
 }
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+      } else{
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $("#topBtn").click(function(){
+      $('html ,body').animate({scrollTop : 0},800);
+    });
+});
