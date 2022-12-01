f749f92eae63c2da2a274b8f385c1840199d74f9
Cadu <99202990+Cadub@users.noreply.github.com>
2022-12-01 16:04:57 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -263,3 +263,17 @@ async function mostrarSenhaLogin(){
     }
 }
 
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+      } else{
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $("#topBtn").click(function(){
+      $('html ,body').animate({scrollTop : 0},800);
+    });
+});
