8bc8123ece9ea7d8c8181e6aa51e73e587f9bd97
Cadu <99202990+Cadub@users.noreply.github.com>
2022-12-01 16:06:44 -0300

    Update perguntar.js

--- js/perguntar.js
+++ js/perguntar.js
@@ -104,3 +104,18 @@ async function sairDaConta(){
 }
 
 inserirUsuario()
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+      } else{
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $("#topBtn").click(function(){
+      $('html ,body').animate({scrollTop : 0},800);
+    });
+});
