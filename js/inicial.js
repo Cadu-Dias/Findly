ef4302ba04325803b6a318f9095a978bd8166f7c
Cadu <99202990+Cadub@users.noreply.github.com>
2022-12-01 16:05:33 -0300

    Update inicial.js

--- js/inicial.js
+++ js/inicial.js
@@ -33,3 +33,18 @@ async function iniciarPergunta() {
 
     window.location.href = "perguntar.html"
 }
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 40){
+        $('#topBtn').fadeIn();
+      } else{
+        $('#topBtn').fadeOut();
+      }
+    });
+  
+    $("#topBtn").click(function(){
+      $('html ,body').animate({scrollTop : 0},800);
+    });
+});
