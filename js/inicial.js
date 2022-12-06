056b0884c35ae2d4b29a7f0fd1fe47930192ee77
Cadu <99202990+Cadub@users.noreply.github.com>
2022-12-06 12:33:24 -0300

    Update inicial.js

--- js/inicial.js
+++ js/inicial.js
@@ -34,6 +34,25 @@ async function iniciarPergunta() {
     window.location.href = "perguntar.html"
 }
 
+async function pesquisarPergunta() {
+
+  const input_pesquisa = document.querySelector('#input-pesquisa')
+  let string_input_pesquisa = input_pesquisa.value
+  
+  if(string_input_pesquisa === "") {
+      Swal.fire({
+          icon: 'error',
+          title: 'Você deve escrever algo para pesquisar a pergunta',
+          showCloseButton: true,
+      });
+  }
+  else{
+      localStorage.setItem("pergunta_pesquisa", string_input_pesquisa)
+      window.location.href = 'disciplinas.html'
+  }
+
+}
+
 $(document).ready(function(){
 
     $(window).scroll(function(){
